5051b8a3cf42bde172fda98c1f52da57922646cd
Wlaister80 <williamlaister@gmail.com>
2024-01-23 23:05:46 -0500

    new validation

---
+++ src/main/java/apiTest/newvalidation.java
@@ -0,0 +1,21 @@
+package apiTest;
+
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class newvalidation {
+    public String urlDeleteRequest = "https://fakerestapi.azurewebsites.net/api/v1/Books/100";
+
+    @Test
+    public void VerifyResponseCodeForDeleteRequest(){
+        given()
+                .when()
+                .delete(urlDeleteRequest)
+                .then()
+                .statusCode(200)
+                .log()
+                .body();
+
+    }
+}
